bcb5acb527eb21ca836451ea7f3225055f2b0fa2
unknown <bishalsharma733@gmail.com>
2022-04-03 16:35:21 +0545

    firs

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Shop from './components/Shop';
+import Navbar from './components/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <div className="container">
+      
+      <Shop/>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/state/store.js
@@ -0,0 +1,4 @@
+import reducers from "./reducers";
+import thunk from "redux-thunk";
+import {applyMiddleware, createStore} from "redux"
+export const store = createStore(reducers,{},applyMiddleware(thunk))
